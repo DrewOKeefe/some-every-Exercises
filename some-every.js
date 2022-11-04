370a09019dcdf8f61d22b1f5f2cf24932dc12ef6
Drew O'Keefe <okeefedrew@gmail.com>
2022-11-04 14:05:01 -0500

    final commit

--- some-every.js
+++ some-every.js
@@ -6,7 +6,13 @@ Examples:
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+	return arr.some(function (num) {
+		if (num % 2 !== 0) {
+			return true;
+		} else return false;
+	});
+}
 
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
@@ -16,7 +22,16 @@ Examples:
     hasAZero(1212121) // false
 */
 
-function hasAZero(num) {}
+function hasAZero(num) {
+	return num
+		.toString()
+		.split('')
+		.some(function (val) {
+			if (val === '0') {
+				return true;
+			} else return false;
+		});
+}
 
 /*
 Write a function called hasOnlyOddNumbers which accepts an array and returns true if every single number in the array is odd. If any of the values in the array are not odd, the function should return false. 
@@ -26,7 +41,13 @@ Examples:
     hasOnlyOddNumbers([1,2,3,5,7]) // false
 */
 
-function hasOnlyOddNumbers(arr) {}
+function hasOnlyOddNumbers(arr) {
+	return arr.every(function (num) {
+		if (num % 2 !== 0) {
+			return true;
+		} else return false;
+	});
+}
 
 /*
 Write a function called hasNoDuplicates which accepts an array and returns true if there are no duplicate values (more than one element in the array that has the same value as another). If there are any duplicates, the function should return false.
@@ -36,7 +57,13 @@ Examples:
     hasNoDuplicates([1,2,3]) // true
 */
 
-function hasNoDuplicates(arr) {}
+function hasNoDuplicates(arr) {
+	return arr.every(function (val) {
+		if (arr.indexOf(val) === arr.lastIndexOf(val)) {
+			return true;
+		} else return false;
+	});
+}
 
 /*
 Write a function called hasCertainKey which accepts an array of objects and a key, and returns true if every single object in the array contains that key. Otherwise it should return false.
@@ -53,7 +80,20 @@ Examples:
     hasCertainKey(arr,'isCatOwner') // false
 */
 
-function hasCertainKey(arr, key) {}
+function hasCertainKey(arr, key) {
+	return arr.every(function (obj) {
+		if (obj[key]) {
+			return true;
+		} else return false;
+	});
+}
+
+// SPRINGBOARD SOLUTION:
+// function hasCertainKey(arr, key){
+//     return arr.every(function(val){
+//       return key in val // WHAT DOES 'in' do?
+//     })
+//   }
 
 /*
 Write a function called hasCertainValue which accepts an array of objects and a key, and a value, and returns true if every single object in the array contains that value for the specific key. Otherwise it should return false.
@@ -71,4 +111,11 @@ Examples:
     
 */
 
-function hasCertainValue(arr, key, searchValue) {}
+function hasCertainValue(arr, key, searchValue) {
+	return arr.every(function (obj) {
+		if (obj[key] !== searchValue) {
+			//why not [searchValue]?
+			return false;
+		} else return true;
+	});
+}
